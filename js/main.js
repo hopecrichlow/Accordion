4c2f701a15f8c9c92fefc05ba4bb89f07bde0bef
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-06 21:44:03 -0400

    Everything working, have to work on transitions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 //IIFE
 (function () {
 
-$('ul').on('click', 'h3', function(){
-  var section = $(event.target).sibling;
-  $(section).removeClass('slide');
-  $(section).addClass('visible')
-});
+$('h3').click(
+  function(){ $(this).parent('one').addClass('hidden') },
+  function(){ $(this).siblings().removeClass('hidden') },
+  function(){ $(this).siblings().addClass('showing') }
+  )
+
+// $('ul').on('click', 'span', function(event){
+//   var header = $(event.target);
+//   header.sibling.removeClass('.hidden');
+//   header.sibling.addClass('.showing');
+// });
 
 
 
